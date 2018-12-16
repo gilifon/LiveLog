799b72809e5eb07eb0acaa836eb792f46c6324ec
Gil (4Z1KD) <gil4z1kd@gmail.com>
2018-12-16 22:34:10 +0200

    change christmas to new year

--- App/main.js
+++ App/main.js
@@ -16,7 +16,7 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (s
     system.debug(true);
     //>>excludeEnd("build");
 
-    app.title = 'Christmas 2019 - Live Log';
+    app.title = 'New Year 2019 - Live Log';
     app.version = "1.0";
 
     app.configurePlugins({
