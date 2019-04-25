52e379cd2ea434ca82d3ce22e5989167ae39bc97
Gil Lianni <gilifon@gmail.com>
2019-04-25 16:38:22 +0300

    enable certificate generator

--- App/viewmodels/kdlog.js
+++ App/viewmodels/kdlog.js
@@ -171,7 +171,7 @@
         //*************************** ref ***************************/
         this.unsordetStations = ko.observableArray([]);
         refData.push(['Station', 'QSOs']);
-        Enumerable.From(statisticsList()).Where(whereClause).Select("$.ses_callsign").OrderBy().Distinct().ForEach(function (s, index) {
+        Enumerable.From(statisticsList()).Where(whereClause).Select("$.ses_callsign").OrderBy("$.id").Distinct().ForEach(function (s, index) {
             var count = Enumerable.From(statisticsList()).Where(function (x) { return x.ses_callsign == s }).Where(whereClause).ToArray().length;
             if (s == null) s = " "
             //unsordetStations.push([s, count]);
